eb7ad0a76ceeaf6b459302b1b5b4a995abeea154
Paolo Murgia <62264946+Musca23@users.noreply.github.com>
2021-06-22 17:25:44 +0200

    renamed loss_functions file to error_functions

--- src/error_functions.py
+++ src/error_functions.py
@@ -3,21 +3,27 @@ import numpy as np
 # TODO use class constructor instead static methods if easier for mapping
 # (string name of the function and pointer to the function)
 
-class LossFunction:
+class ErrorFunction:
+    """
+    Functions for the "data term" to evaluate model error.
+    """
 
     @staticmethod
-    def squared_loss(prediction, target):
+    def squared_error(prediction, target):
         # TODO check if it is better to compute the mean inside the function 
         # be careful of types passed as parameters 
         return (np.dot(target - prediction, target - prediction))/2     # /2 usefull for the derivation
     
     @staticmethod  
-    def square_loss_der(prediction, target):
+    def squared_error_der(prediction, target):
         return (target - prediction)
 
     @staticmethod
-    def euclidean_loss(prediction, target):
-        return np.linalg.norm(prediction - target)
+    def euclidean_error(prediction, target):
+        return np.linalg.norm(prediction - target) #L2 norm
 
     @staticmethod
-    def euclidian_loss_der(prediction, target):
+    def euclidian_error_der(prediction, target):
+        return (prediction - target) / ErrorFunction.euclidean_loss(prediction, target)
+
+
