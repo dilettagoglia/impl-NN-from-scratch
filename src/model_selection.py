18e8e654429f27855adfd34804b1b443cd68d4ba
Diletta Goglia <diletta.goglia@gmail.com>
2021-09-29 16:38:23 +0200

    Grid search impostata

--- src/model_selection.py
+++ src/model_selection.py
@@ -3,8 +3,15 @@ from utility import *
 import numpy as np
 import tqdm
 from net import Network
+import json
+from joblib import Parallel, delayed
+import numpy as np
+import os
+from collections import OrderedDict
+import random
+import itertools as it
 
-# TODO implement cross-validation, grid search and random search inside this class
+''' *** HOLD OUT *** '''
 
 # split a dataset into a train and validation set
 def holdout_validation(net,path, test_size, loss, metr, lr, shuffle=True, lr_decay=None, limit_step=None, decay_rate=None, decay_steps=None,
@@ -30,6 +37,8 @@ def holdout_validation(net,path, test_size, loss, metr, lr, shuffle=True, lr_dec
                     nesterov=nesterov, reg_type=reg_type, lambda_=lambda_)
     tr_err, tr_metr, val_err, val_metr = net.fit(tr_x=train_X, tr_y=train_Y, val_x=val_X, val_y=val_Y, batch_size=batch_size, epochs=epochs, strip_early_stopping=strip)
     return tr_err, tr_metr, val_err, val_metr
+
+''' *** K-FOLD CV *** '''
 
 #TODO DocString documentation
 def kfold_CV(net, dataset, loss, metr, lr, path=None, lr_decay=None, limit_step=None, decay_rate=None, decay_steps=None,
@@ -128,4 +137,247 @@ def kfold_CV(net, dataset, loss, metr, lr, path=None, lr_decay=None, limit_step=
         plot_curves(tr_error_values, val_error_values, tr_metric_values, val_metric_values, path, ylim=ylim,
                     lbltr=lbltr, lblval=lblval)
     return avg_val_err, std_val_err, avg_val_metric, std_val_metric
-    
+
+''' *** USEFUL FUNCTIONS FOR GRID SEARCH *** '''
+
+def randomize_params(base_params, fb_dim, n_config=2):
+    """
+    Generates combination of random hyperparameters
+    :param base_params: parameters on which the random perturbation will be applied
+    :param fb_dim: full batch dimension (expressed as a number)
+    :param n_config: number of random configurations to be generated for each hyper-parameter
+    :return: combos of randomly generated hyper-parameters' values
+    """
+    n_config -= 1
+    rand_params = {}
+    for k, v in base_params.items():
+        # if the parameter does not have to change
+        if k in ('act_functions', 'weights_init', 'decay_rate', 'loss', 'lr_decay', 'metr', 'reg_type', 'staircase',
+                 'units_per_layer', 'limits', 'epochs'):
+            rand_params[k] = (v,)
+        else:
+            rand_params[k] = [v]
+            for i in range(n_config):
+                # generate n_config random value centered in v
+                if k == "batch_size":
+                    if v == "full":
+                        rand_params[k] = ("full",)
+                        continue
+                    lower = max(v - 15, 1)
+                    upper = min(v + 15, fb_dim)
+                    value = random.randint(lower, upper)
+                    for bs in rand_params[k]:
+                        if abs(value - bs) < 5:
+                            value = rand_params[k][0]
+                    while value in rand_params[k]:
+                        lower = max(v - 15, 1)
+                        upper = min(v + 15, fb_dim)
+                        value = random.randint(lower, upper)
+                        for bs in rand_params[k]:
+                            if abs(value - bs) < 5:
+                                value = rand_params[k][0]
+                    rand_params[k].append(value)
+
+                # elif k == "decay_rate":
+                #     if v is not None:
+                #         lower = max(0., v - 0.2)
+                #         upper = min(1., v + 0.2)
+                #         rand_params[k].append(random.uniform(lower, upper))
+                #     else:
+                #         rand_params[k] = (None,)
+
+                elif k in ("limit_step", "decay_steps"):
+                    if base_params['lr_decay'] is not None:
+                        if v is None or (base_params['lr_decay'] == 'linear' and k == 'decay_steps') or \
+                                (base_params['lr_decay'] == 'exponential' and k == 'limit_step'):
+                            rand_params[k] = (None,)
+                            continue
+                        lower = max(1, v - 100)
+                        upper = v + 100
+                        rand_params[k].append(random.randint(lower, upper))
+                    else:
+                        rand_params[k] = (v,)
+
+                elif k in ("lambd", "lr"):
+                    value = max(0., np.random.normal(loc=v, scale=0.001))
+                    for l in rand_params[k]:
+                        if abs(value - l) < 0.0005:
+                            value = rand_params[k][0]
+                    while value in rand_params[k]:
+                        value = max(0., np.random.normal(loc=v, scale=0.001))
+                        for l in rand_params[k]:
+                            if abs(value - l) < 0.0005:
+                                value = rand_params[k][0]
+                    rand_params[k].append(value)
+
+                # elif k == "limits":
+                #     lower, upper = v[0], v[1]
+                #     lower = np.random.normal(loc=lower, scale=0.1)
+                #     upper = np.random.normal(loc=upper, scale=0.1)
+                #     if lower > upper:
+                #         aux = lower
+                #         lower = upper
+                #         upper = aux
+                #     rand_params[k].append((lower, upper))
+
+                elif k == "momentum":
+                    value = max(0., np.random.normal(loc=v, scale=0.1))
+                    for m in rand_params[k]:
+                        if abs(value - m) < 0.05:
+                            value = rand_params[k][0]
+                    while value in rand_params[k] or value > 1.:
+                        value = min(1., np.random.normal(loc=v, scale=0.1))
+                        for m in rand_params[k]:
+                            if abs(value - m) < 0.05:
+                                value = rand_params[k][0]
+                    rand_params[k].append(value)
+
+    print(rand_params)
+    return rand_params
+
+def list_of_combos(param_dict):
+    """
+    Takes a dictionary with the combinations of params to use in the grid search and creates a list of dictionaries, one
+    for each combination (so it's possible to iterate over this list in the GS, instead of having many nested loops)
+    :param param_dict: dict{kind_of_param: tuple of all the values of that param to try in the grid search)
+    :return: list of dictionaries{kind_of_param: value of that param}
+    """
+    expected_keys = sorted(['units_per_layer', 'act_functions', 'weights_init', 'limits', 'momentum', 'batch_size', 'lr', 'loss',
+                            'metr', 'epochs', 'lr_decay', 'decay_rate', 'decay_steps', 'staircase', 'limit_step',
+                            'lambd', 'reg_type'])
+    for k in expected_keys:
+        if k not in param_dict.keys():
+            param_dict[k] = ('l2',) if k == 'reg_type' else ((0,) if k == 'lambd' else (None,))
+    param_dict = OrderedDict(sorted(param_dict.items()))
+    combo_list = list(it.product(*(param_dict[k] for k in param_dict.keys())))
+    combos = []
+    for c in combo_list:
+        if len(c[expected_keys.index('units_per_layer')]) == len(c[expected_keys.index('act_functions')]):
+            d = {k: c[i] for k, i in zip(expected_keys, range(len(expected_keys)))}
+            combos.append(d)
+
+    for c in combos:
+        if c['lr_decay'] == 'linear':
+            c['decay_rate'] = None
+            c['decay_steps'] = None
+            c['staircase'] = False
+        elif c['lr_decay'] == 'exponential':
+            c['limit_step'] = None
+
+    final_combos = []
+    for i in range(len(combos) - 1):
+        if combos[i] != combos[i + 1]:
+            final_combos.append(combos[i])
+
+    return final_combos
+
+def get_best_models(dataset, coarse=False, n_models=1, fn=None):
+    """
+    Search and select the best models based on the MEE metric and standard deviation
+    :param dataset: name of the dataset (used for reading the file containing the results)
+    :param coarse: indicates if best models result from a coarse or fine grid search (used for reading the file name)
+    :param n_models: number of best models to be returned
+    :param fn: file name for reading a specific file for the results (if different from the default)
+    :return: best models in term of MEE and standard deviation and their parameters
+    """
+    file_name = ("coarse_gs_" if coarse else "fine_gs_") + "results_" + dataset + ".json"
+    file_name = file_name if fn is None else fn
+    with open("../results/" + file_name, 'r') as f:
+        data = json.load(f)
+
+    # put the data into apposite lists
+    input_dim = 10 if dataset == "cup" else 17
+    models, params, errors, std_errors, metrics, std_metrics = [], [], [], [], [], []
+    for result in data['results']:
+        if result is not None:
+            errors.append(round(result[0], 3))
+            std_errors.append(round(result[1], 3))
+            metrics.append(round(result[2], 3))
+            std_metrics.append(round(result[3], 3))
+
+    errors, std_errors = np.array(errors), np.array(std_errors)
+    metrics, std_metrics = np.array(metrics), np.array(std_metrics)
+    for i in range(n_models):
+        # find best metric model and its index
+        index_of_best = np.argmin(metrics) if dataset == "cup" else np.argmax(metrics)
+        value_of_best = min(metrics) if dataset == "cup" else max(metrics)
+
+        # search elements with the same value
+        if len(metrics) > index_of_best + 1:
+            indexes = [index_of_best]
+            for j in range(index_of_best + 1, len(metrics)):
+                if metrics[j] == value_of_best:
+                    indexes.append(j)
+
+            std_metr_to_check = std_metrics[indexes]
+            value_of_best = min(std_metr_to_check)
+            index_of_best = indexes[np.argmin(std_metr_to_check)]
+            for j in indexes:
+                if std_metrics[j] != value_of_best:
+                    indexes.remove(j)
+
+            err_to_check = errors[indexes]
+            value_of_best = min(err_to_check)
+            index_of_best = indexes[np.argmin(err_to_check)]
+            for j in indexes:
+                if errors[j] != value_of_best:
+                    indexes.remove(j)
+
+            std_err_to_check = std_errors[indexes]
+            value_of_best = min(std_err_to_check)
+            index_of_best = indexes[np.argmin(std_err_to_check)]
+            for j in indexes:
+                if std_errors[j] != value_of_best:
+                    indexes.remove(j)
+
+        metrics = np.delete(metrics, index_of_best)
+        models.append(Network(input_dim=input_dim, **data['params'][index_of_best]))
+        params.append(data['params'][index_of_best])
+
+    return models, params
+
+''' *** GRID SEARCH *** '''
+
+def grid_search(dataset, params, coarse=True, n_config=1):
+    """
+    Performs a grid search over a set of parameters to find the best combination of hyperparameters
+    :param dataset: name of the dataset (monks-1, monks-2, monks-3, cup)
+    :param params: dictionary with all the values of the params to try in the grid search
+    :param coarse: (bool) if True perform a gird search only on the values of 'params'
+    :param n_config: (int) number of config to generate for each param in case of NOT coarse grid search
+    """
+    models = []
+    # input_dim = 10 if dataset == "cup" else 17
+    input_dim = 17 #TODO: modifica temporanea
+
+    # In case generate random combinations
+    if not coarse:
+        params = randomize_params(params, dataset, n_config)
+
+    # generate list of combinations
+    param_combos = list_of_combos(params)
+    print(f"Total number of trials: {len(param_combos)}")
+    for combo in param_combos:
+        models.append(Network(input_dim=input_dim, **combo))
+
+    # perform parallelized grid search
+    results = Parallel(n_jobs=os.cpu_count(), verbose=50)(delayed(kfold_CV)(
+        net=models[i], dataset=dataset, k_folds=5, disable_tqdm=(True, True), interplot=False,
+        **param_combos[i]) for i in range(len(param_combos)))
+
+    # do not save models with suppressed training
+    for r, p in zip(results, param_combos):
+        if r is None:
+            results.remove(r)
+            param_combos.remove(p)
+
+    # write results on file
+    folder_path = "../results/"
+    file_name = ("coarse_gs_" if coarse else "fine_gs_") + "results_" + dataset + ".json"
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+    data = {"params": param_combos, "results": results}
+    with open(folder_path + file_name, 'w') as f:
+        json.dump(data, f, indent='\t')
+
+
--- src/run.py
+++ src/run.py
@@ -2,6 +2,7 @@ from net import Network
 from utility import *
 from sklearn.model_selection import train_test_split
 import numpy as np
+from model_selection import kfold_CV
 
 
 if __name__ == '__main__':
@@ -17,15 +18,20 @@ if __name__ == '__main__':
     monk_train, labels_tr = read_monk_dataset(dataset=tr_ds_name, rescale=True, preliminary_analysis=False)
     #monk_test, labels_ts = read_monk_dataset(dataset="monks-1.test")
 
-    # hold-out validation
-    train_X, val_X, train_y, val_y = train_test_split(monk_train, labels_tr, test_size=0.30)
+    '''********* hold-out validation ************** '''
+    #train_X, val_X, train_y, val_y = train_test_split(monk_train, labels_tr, test_size=0.30)
 
-    model.compile(error_func='squared_error', metr='binary_class_accuracy', lr=0.07, momentum=0.7, lambda_=0.0001)
+    #model.compile(error_func='squared_error', metr='binary_class_accuracy', lr=0.07, momentum=0.7, lambda_=0.0001)
 
-    tr_err, tr_metr, val_err, val_metr = model.fit(tr_x=train_X, tr_y=train_y, val_x=val_X, val_y=val_y, batch_size='full',
-                                                    epochs=150, tqdm=True)
+    #tr_err, tr_metr, val_err, val_metr = model.fit(tr_x=train_X, tr_y=train_y, val_x=val_X, val_y=val_y, batch_size='full', epochs=150, tqdm=True)
+
+    ''' ********** cross validation ***************** '''
+    #kfold_CV(net=model, dataset=tr_ds_name, loss='squared_error', metr='binary_class_accuracy', lr=0.76, opt='sgd', momentum=0.83,
+                #epochs = 500, batch_size = 'full', k_folds = 5, disable_tqdms = (True, False), plot = True, verbose = True)
+
+
     # # plot the learning curves
-    plot_curves(tr_err, val_err, tr_metr, val_metr, lbltr='Training', lblval='Validation')
+    # plot_curves(tr_err, val_err, tr_metr, val_metr, lbltr='Training', lblval='Validation')
 
     # model.print_topology()
 
