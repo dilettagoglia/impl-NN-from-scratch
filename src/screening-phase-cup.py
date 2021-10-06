febe18c18c6790e594d2c9cc801f9f9c7075d0c3
Paolo Murgia <62264946+Musca23@users.noreply.github.com>
2021-10-06 21:43:53 +0200

    added screening phase with some comparisons

--- src/screening-phase-cup.py
+++ src/screening-phase-cup.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     metr = 'euclidian_error'
     reg_type = 'ridge_regression'  # default regularization type is Tikhonov
     batch_size = 128 # try out batch sizes in powers of 2 (for better memory optimization) based on the data-size
-    epochs = 250
+    epochs = 1
     lr = 0.01
     # lr_val = [0.001, 0.005, 0.01, 0.05]
     momentum = 0.6
@@ -42,14 +42,16 @@ if __name__ == '__main__':
         for j in lambda_val:
             model = Network(input_dim=10, units_per_layer=i, act_functions=z, weights_init=weights_init)
             model.compile(error_func=error_func, metr=metr, lr=lr, momentum=momentum, nesterov=True, lambda_=j)
-
+            
+            if j == 0.1 and i == [20,20,20,20,2]:
+                epochs = 250
             # # training (check the method definition for more info about all the possible parameters)
             tr_err, tr_metr, val_err, val_metr = model.fit(tr_x=tr_data, tr_y=tr_targets, val_x=int_ts_data, val_y=int_ts_targets,
                                                            batch_size=batch_size,
                                                            epochs=epochs, disable_tqdm=False)
             ylim = 30
-            if tr_err[50] > ylim or val_err[50] > ylim:
-                ylim = 100
+            # if tr_err[50] > ylim or val_err[50] > ylim:
+            ylim = 200
             # plot_curves(tr_err, val_err, tr_metr, val_metr, lbltr='Training', lblval='Validation',ylim=ylim)
             
             # to plot the loss curve: substitute tr_metr with tr_err and val_metr with val_err
@@ -66,4 +68,4 @@ if __name__ == '__main__':
 
     plt.suptitle(f'Units per layer and lambda variations with'
                  f' momentum = {str(momentum)}, learning_rate = {str(lr)}, act_functions = tanh for hidden layer, weights_init = {str(weights_init)}, \n loss = {str(error_func)}, metr = {str(metr)}, batch_size = {str(batch_size)}, epochs = {str(epochs)}', fontsize=28, fontweight='bold')
-    plt.savefig(f'../images/cup_screening_phase_cmp_unitslayer_lambda.png')
+    plt.savefig(f'../images/cup_screening_phase.png')
