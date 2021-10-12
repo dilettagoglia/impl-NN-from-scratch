ee79d7ca166a99d3de74f636f0b5c1e35ee3d5d9
Paolo Murgia <62264946+Musca23@users.noreply.github.com>
2021-10-12 11:56:30 +0200

    .

--- src/model_selection_cup_test.py
+++ src/model_selection_cup_test.py
@@ -33,5 +33,7 @@ if __name__ == '__main__':
 
     ''' FINE '''
     #grid_search(dataset="cup", params=best_params, coarse=False, n_config=3, baseline_es=baseline_es)
-    best_models, best_params = get_best_models(dataset="cup", coarse=False, n_models=5, fn='fine_gs_results_cup_diletta')
-    print(best_models)
+    best_models, best_params = get_best_models(dataset="cup", coarse=False, n_models=5, fn='fine_gs_results_cup_musca.json')
+    best_params = best_params[0]
+    for p, v in best_params.items():
+        print(p, v)
