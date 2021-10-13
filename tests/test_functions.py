a1654f921a7873a1c50bde2d38fcfe549d936420
Paolo Murgia <62264946+Musca23@users.noreply.github.com>
2021-10-13 22:27:52 +0200

    cleanup folder

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -3,8 +3,6 @@ import numpy as np
 from src.activation_functions import ActivationFunction
 import torch
 
-# TODO implement unittest for each function later with numpy.testing and PyTorch function
-# and configure Travis for continuous integration in GitHub (if we have time and desire)
 
 class TestFunctions(unittest.TestCase):
 
