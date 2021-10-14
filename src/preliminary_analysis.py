318c0240905a73a67d6479ff1923d922c1d2f07b
Diletta Goglia <diletta.goglia@gmail.com>
2021-10-14 18:27:20 +0200

    Update preliminary_analysis.py

--- src/preliminary_analysis.py
+++ src/preliminary_analysis.py
@@ -3,6 +3,9 @@ matplotlib.rcParams['figure.figsize'] = (10.0, 10.0)
 from utility import *
 from net import *
 import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn import metrics
 
 # Monk train dataset analysis
 ds_names=['monks-1.train', 'monks-2.train', 'monks-3.train']
@@ -23,21 +26,13 @@ print('\nLabels vector shape:\n', tr_targets.shape, '\n Training dataset shape: 
 
 
 ''' BIAS VARIANCE DECOMPOSITION'''
-
-#This library is used to decompose bias and variance in our models
-#from mlxtend.evaluate import bias_variance_decomp
-
-import warnings
-warnings.filterwarnings('ignore')
-from sklearn import metrics
-
 epochs = 250
-batch_size=256
+batch_size = 145
 
 devset_x, devset_y, int_ts_x, int_ts_y, ts_data = read_cup(int_ts=True)
 # Model definition
-model = Network(input_dim=10, units_per_layer=[20, 2], act_functions=['tanh', 'identity'], weights_init='glorot')
-model.compile(error_func='squared_error', metr='euclidian_error', lr=0.001, lr_decay=None,momentum=0.6, nesterov=True, lambda_=0.001, reg_type='ridge_regression')
+model = Network(input_dim=10, units_per_layer=[20, 20,  2], act_functions=['relu', 'relu', 'identity'], weights_init='glorot')
+model.compile(error_func='squared_error', metr='euclidian_error', lr=0.001, lr_decay=None, momentum=0.6, nesterov=True, lambda_=0, reg_type='ridge_regression')
 
 # Estimation of bias and variance using bias_variance_decomp
 def _draw_bootstrap_sample(rng, X, y):
@@ -47,38 +42,30 @@ def _draw_bootstrap_sample(rng, X, y):
                                    replace=True)
     return X[bootstrap_indices], y[bootstrap_indices]
 
-def bias_variance_decomp(estimator, X_train, y_train, X_test, y_test,
-                         loss='mse', num_rounds=200, random_seed=None,
-                         **fit_params):
+def bias_variance_decomp(estimator, X_train, y_train, X_test, y_test, num_rounds=50, random_seed=None, **fit_params):
     rng = np.random.RandomState(random_seed)
-    dtype = np.float64
-    all_pred = np.zeros((num_rounds, y_test.shape[0]), dtype=dtype)
+    all_pred = []
 
     for i in range(num_rounds):
         X_boot, y_boot = _draw_bootstrap_sample(rng, X_train, y_train)
 
         pred = estimator.fit(X_boot, y_boot, **fit_params).predict(X_test)
-        all_pred[i] = pred # todo fix error
-
-    avg_expected_loss = np.apply_along_axis(
-        lambda x:
-        ((x - y_test) ** 2).mean(),
-        axis=1,
-        arr=all_pred).mean()
+        all_pred.append(pred)
 
     main_predictions = np.mean(all_pred, axis=0)
 
     avg_bias = np.sum((main_predictions - y_test) ** 2) / y_test.size
-    avg_var = np.sum((main_predictions - all_pred) ** 2) / all_pred.size
+    avg_var = np.sum((main_predictions - all_pred) ** 2) / len(all_pred)
 
-    return avg_expected_loss, avg_bias, avg_var
+    return avg_bias, avg_var
 
-#Note here we are using loss as 'mse' and setting default bootstrap num_rounds to 200
-mse, bias, var = bias_variance_decomp(model, devset_x, devset_y, int_ts_x, int_ts_y, loss='mse', num_rounds=200, random_seed=123, epochs=epochs, batch_size=batch_size, error_exp=True)
+
+# Note here we are using loss as 'mse' and setting default bootstrap num_rounds to 200
+bias, var = bias_variance_decomp(model, devset_x, devset_y, int_ts_x, int_ts_y, num_rounds=50, random_seed=123,
+                                      epochs=epochs, batch_size=batch_size, error_exp=True)
 
 y_pred = model.predict(int_ts_x)
 # summarize results
-print('MSE from bias_variance lib [avg expected loss]: %.3f' % mse)
 print('Avg Bias: %.3f' % bias)
 print('Avg Variance: %.3f' % var)
 print('Mean Square error by Sckit-learn lib: %.3f' % metrics.mean_squared_error(int_ts_y, y_pred))
